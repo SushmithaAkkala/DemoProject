8374bcacc4def9b629c4b00389ed3fb45aedfd24
Naveen Neelam <sushmitha.akkala@gmail.com>
2024-04-25 18:23:21 +0530

    this is my first commit

---
+++ ArithmaticOperator.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15;
+		int b=10;
+		System.out.println(a+b);//15+10=25
+		System.out.println(a-b);//15-10=5
+	    System.out.println(a*b);//15*10=150
+	    System.out.println(a/b);//15/10=1
+	    System.out.println(a%b);//15%10=5
+
+
+	}
+
+}
