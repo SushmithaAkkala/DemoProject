5bfb145e5a121d977c55bd9bb0f879f877d27280
Naveen Neelam <sushmitha.akkala@gmail.com>
2024-04-25 18:54:15 +0530

    Java Operators

---
+++ InvertingOperator.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class InvertingOperator {
+
+	public static void main(String[] args) {
+		int a=15;
+		int d=-3;
+		
+		System.out.println(~a);//-16(increases by one)
+		System.out.println(~d);//2(decreases by one and sign changes)
+	}
+
+}
---
+++ UnaryOperators.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15;
+		System.out.println(a++);//a=15
+		System.out.println(a);//a=16
+		System.out.println(++a);//a=17
+		System.out.println(a);//a=17
+		System.out.println(a--);//a=17
+		System.out.println(a);//a=16
+		System.out.println(--a);//a=15
+
+
+	}
+
+}
